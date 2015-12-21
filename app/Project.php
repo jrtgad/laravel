9dbccc9b890e8e257a9eefe6c89d4a2cf104eb17
2daw <2daw@A101PC1>
2015-12-21 19:58:22 +0100

    Merge branch 'master' of https://github.com/jrtgad/laravel
    # Conflicts:
    #	composer.json

--- app/Project.php
+++ app/Project.php
@@ -5,5 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model {
+
     //
+
+    public function tasks() {
+        return $this->hasMany('App\Task');
+    }
+
 }
--- app/Task.php
+++ app/Task.php
@@ -5,5 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model {
+
     //
+
+    public function project() {
+        return $this->belongsTo('App\Project');
+    }
+
 }
